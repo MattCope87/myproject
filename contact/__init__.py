7a425a4b3a193cd9cf2bf57c2819b636ff8dd10e
MattCope87 <145110001+MattCope87@users.noreply.github.com>
2023-12-07 16:56:01 +0000

    mercy commit 1

--- contact/__init__.py
+++ contact/__init__.py
@@ -1,23 +1,30 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for 
 
+app = Flask(__name__, instance_relative_config=True) 
+app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'contact.sqlite'),
+    )
 
-app = Flask(__name__) 
-# create global data variable
-global_data = []
+
+
 
 @app.route("/", methods=["GET"]) 
 def home(): 
+	
 	return render_template('contact.html')
-
+	
 @app.route("/result", methods=["POST"]) 
 def result(): 
 	# append to data container
 		
-	
-
 	data = [request.form['fullname'], request.form['email'], request.form['message']]
-	global_data.append(data)
+	#global_data.append(data)
 	#fullname = request.form['fullname']
 	
 	return render_template('result.html', data = data)
 
+
+from . import db
+db.init_app(app)
