1328c16bbbbd554af675a84ce5d18e023836359c
MattCope87 <145110001+MattCope87@users.noreply.github.com>
2023-12-07 17:33:58 +0000

    second commit

--- contact/__init__.py
+++ contact/__init__.py
@@ -1,30 +1,45 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for 
 
-app = Flask(__name__, instance_relative_config=True) 
-app.config.from_mapping(
+def create_app(test_config=None):
+	app = Flask(__name__, instance_relative_config=True) 
+	app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'contact.sqlite'),
     )
+	if test_config is None:
+        # load the instance config, if it exists, when not testing
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+        # load the test config if passed in
+		app.config.from_mapping(test_config)
+	try:
+    	os.makedirs(app.instance_path)
 
-
-
-
-@app.route("/", methods=["GET"]) 
-def home(): 
+    # ensure the instance folder exists
+	except OSError:
+        pass
 	
-	return render_template('contact.html')
+	@app.route('/home')
+	def home():
+		return 'Hello Hello'
+
+	from . import db
+	db.init_app(app)
+ 
+	return app
+
+# @app.route("/", methods=["GET"]) 
+# def home(): 
 	
-@app.route("/result", methods=["POST"]) 
-def result(): 
-	# append to data container
+# 	return render_template('contact.html')
+	
+# @app.route("/result", methods=["POST"]) 
+# def result(): 
+# 	# append to data container
 		
-	data = [request.form['fullname'], request.form['email'], request.form['message']]
-	#global_data.append(data)
-	#fullname = request.form['fullname']
+# 	data = [request.form['fullname'], request.form['email'], request.form['message']]
+# 	#global_data.append(data)
+# 	#fullname = request.form['fullname']
 	
-	return render_template('result.html', data = data)
-
-
-from . import db
-db.init_app(app)
+# 	return render_template('result.html', data = data)
